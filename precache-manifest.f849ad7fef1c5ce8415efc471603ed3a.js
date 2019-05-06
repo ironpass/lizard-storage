0ce64c3111c88fb8de2c8cb5e550cd177037b0ca
Akarachai Passavoranan (Ood) <akarachai45525@gmail.com>
2019-05-06 20:37:39 +0700

    first commit

---
+++ precache-manifest.f849ad7fef1c5ce8415efc471603ed3a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c866f12609fd6e607712923cb2a6066",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb30788f7736c02d3ce7",
+    "url": "./static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0b3d5409e1b85e1bc503",
+    "url": "./static/css/main.097c1d4f.chunk.css"
+  },
+  {
+    "revision": "cb30788f7736c02d3ce7",
+    "url": "./static/js/2.029572fa.chunk.js"
+  },
+  {
+    "revision": "0b3d5409e1b85e1bc503",
+    "url": "./static/js/main.1f0c6dbf.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b3aa4b4bce41e1f1b23d26aabb0b5e53",
+    "url": "./static/media/download.b3aa4b4b.svg"
+  },
+  {
+    "revision": "d56c869e93805e2c29de6f858318c8c5",
+    "url": "./static/media/lizard.d56c869e.svg"
+  },
+  {
+    "revision": "5bd80917a9f129a6d60e870d9788ec95",
+    "url": "./static/media/trash.5bd80917.svg"
+  },
+  {
+    "revision": "893e3b658ce5d65612cfa6cedb575326",
+    "url": "./static/media/upload.893e3b65.svg"
+  }
+]);
